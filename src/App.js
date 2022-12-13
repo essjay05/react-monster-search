5a60b0c64167bce120aabfeebd14e7830fe651dc
Joy Serquiña <stepjoyous@gmail.com>
2022-12-13 11:00:36 -0800

    Built out monsters list and successfully implemented search and optimized it.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      monsters: [],
+      searchTerm: ''
+    }
+
+    console.log('constructor')
+  }
+
+  // lifecycle methods
+  componentDidMount() {
+    console.log('componentDidMount')
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState(
+        () => {
+          return { monsters: users}
+        },
+        () => {
+          console.log('monsters retrieved')
+          console.log(this.state)
+        }
+      ))
+  }
+
+  onSearchChange = (event) => {
+    console.log(event.target.value)
+    const searchTerm = event.target.value.toLowerCase()
+    this.setState(() => {
+      return { searchTerm }
+    })
+  }
+
+  render() {
+
+    console.log('render')
+
+    const { monsters, searchTerm } = this.state
+    const { onSearchChange } = this
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchTerm)
+    })
+
+    return (
+      <div className='App'>
+        <input
+          className='search-box' 
+          type='search' 
+          placeholder='Search monsters' 
+          onChange={ onSearchChange }/>
+        {
+          filteredMonsters.map((monster) => {
+            return (
+              <div key={ monster.id }>
+                <h1>{ monster.name }</h1>
+              </div>
+            )
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
